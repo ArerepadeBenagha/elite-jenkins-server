22979d7f5929f9b9982a7ae6b30d9097d56a241a
ArerepadeB <aredelulu@gmail.com>
2021-12-12 17:39:14 -0600

    adding listener for https

--- jenkins-dev/main.tf
+++ jenkins-dev/main.tf
@@ -66,7 +66,7 @@ resource "aws_lb_listener" "jenkins_lblist2" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = "arn:aws:acm:us-east-1:901445516958:certificate/f84416e3-4aef-4a99-b522-32f3a7c8fbd2"
+  certificate_arn   = "arn:aws:acm:us-east-1:901445516958:certificate/f01d0c52-1ee7-45b0-b268-5b6ef4db876f"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.jenkins_tglb.arn
