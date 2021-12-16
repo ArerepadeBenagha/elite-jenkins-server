ba70234c5489539d94f990eecf04ef491c84b53f
ArerepadeB <aredelulu@gmail.com>
2021-12-16 17:44:02 -0600

    commenting out listener

--- jenkins-dev/main.tf
+++ jenkins-dev/main.tf
@@ -60,18 +60,18 @@ resource "aws_lb_target_group_attachment" "jenkins_tglbat" {
   port             = 8080
 }
 
-# ####-------- SSL Cert ------#####
-resource "aws_lb_listener" "jenkins_lblist2" {
-  load_balancer_arn = aws_lb.jenkinslb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = "arn:aws:acm:us-east-1:901445516958:certificate/087594ad-3f21-4bec-a8d6-00aae7fffdcc"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins_tglb.arn
-  }
-}
+# # ####-------- SSL Cert ------#####
+# resource "aws_lb_listener" "jenkins_lblist2" {
+#   load_balancer_arn = aws_lb.jenkinslb.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+#   certificate_arn   = "arn:aws:acm:us-east-1:901445516958:certificate/087594ad-3f21-4bec-a8d6-00aae7fffdcc"
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.jenkins_tglb.arn
+#   }
+# }
 
 
 ####---- Redirect Rule -----####
