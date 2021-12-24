22b29dec2798c9ec5743d1659e8e833895951a92
ArerepadeB <aredelulu@gmail.com>
2021-12-24 08:08:59 -0600

    adding sonar  to backend

--- jenkins-dev/main.tf
+++ jenkins-dev/main.tf
@@ -94,7 +94,7 @@ resource "aws_lb_listener" "jenkins_lblist2" {
   certificate_arn   = "arn:aws:acm:us-east-1:375866976303:certificate/f3e1c14c-94cb-4c7f-b150-df5996c52f18"
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.jenkins_tglb.arn
+    target_group_arn = [aws_lb_target_group.jenkins_tglb.arn,aws_lb_target_group.sonar_tglb.arn]
   }
 }
 
