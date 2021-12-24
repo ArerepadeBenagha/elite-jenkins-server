d097211a1d07d963a0f83de7e0cecc52df590111
ArerepadeB <aredelulu@gmail.com>
2021-12-24 07:28:03 -0600

    adding sonar  to backend

--- jenkins-dev/main.tf
+++ jenkins-dev/main.tf
@@ -20,7 +20,7 @@ resource "aws_lb" "jenkinslb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ec2-sg.id, aws_security_group.main-alb.id]
   subnets            = [aws_subnet.main-public-1.id, aws_subnet.main-public-2.id]
-  idle_timeout       = "60"
+  idle_timeout       = "120"
 
   access_logs {
     bucket  = aws_s3_bucket.logs_s3dev.bucket
