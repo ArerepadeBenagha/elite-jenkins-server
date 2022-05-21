5956be8265e1e59de09ef9803385391b5c6a3a56
Louis Benagha <80588773+ArerepadeBenagha@users.noreply.github.com>
2022-05-21 15:48:37 -0500

    Update main.tf

--- simple-server/main.tf
+++ simple-server/main.tf
@@ -1,5 +1,6 @@
 ###########------ simple Server -----########
 resource "aws_instance" "simpleserver" {
+  count                  = 3
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.main-public-1.id
